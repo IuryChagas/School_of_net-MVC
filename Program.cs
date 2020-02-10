dcefc9f56c9cb3cd203374100e1644a40391fd32
Iury Chagas <iurychagas@icloud.com>
2020-02-10 10:56:34 -0300

    add .gitignore
    Conhecendo alguns comandos básicos
    dotnet --help
    dotnet --version
    dotnet new --help
    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace School_of_net_MVC {
+    class Program {
+        static void Main (string[] args) {
+            bool hellWorld = true;
+
+            if (hellWorld) {
+                Console.WriteLine ("Hello World!");
+            }else{
+                Console.WriteLine ("Bye World!");
+            }
+
+        }
+    }
+}
