78ca775c79b4a4df6998a746b6da3b396dca3105
Iury Chagas <iurychagas@icloud.com>
2020-02-10 11:29:28 -0300

    add intro MVC directories

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace School_of_net_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace School_of_net_MVC {
-    class Program {
-        static void Main (string[] args) {
-            bool hellWorld = true;
-
-            if (hellWorld) {
-                Console.WriteLine ("Hello World!");
-            }else{
-                Console.WriteLine ("Bye World!");
-            }
-
+    public class Program {
+        public static void Main (string[] args) {
+            CreateHostBuilder (args).Build ().Run ();
         }
+
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder => {
+                webBuilder.UseStartup<Startup> ();
+            });
     }
 }
