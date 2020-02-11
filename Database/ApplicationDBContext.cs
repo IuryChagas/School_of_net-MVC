71eaee20e111581a26803a795098815784ca4358
Iury Chagas <iurychagas@icloud.com>
2020-02-11 15:48:30 -0300

    add Subindo a migração!
    Introdução ao módulo 3
    > Criado nova categoria no Banco de Dados

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -6,6 +6,7 @@ namespace School_of_net_MVC.Database {
     public class ApplicationDBContext : DbContext {
 
         public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Categoria>  Categorias { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
             
         }
---
+++ Models/Categoria.cs
@@ -0,0 +1,6 @@
+namespace School_of_net_MVC.Models {
+    public class Categoria {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
