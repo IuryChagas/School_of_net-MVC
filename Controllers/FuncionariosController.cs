b47d3e04a5ff07cafd7d40ea6d2ef0020eb1f395
Iury Chagas <iurychagas@icloud.com>
2020-02-11 13:08:07 -0300

    add Salvando dados no Banco de Dados
    > Injeção de dependencia
    > Consumindo serviços
    > Add e Save Dados no BD

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -2,16 +2,25 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using School_of_net_MVC.Models;
 using System.Globalization;
+using School_of_net_MVC.Database;
 
 namespace School_of_net_MVC.Controllers {
     public class FuncionariosController : Controller {
+
+        private readonly ApplicationDBContext database;
+
+        public FuncionariosController(ApplicationDBContext database){
+            this.database = database;
+        }
         public IActionResult Cadastrar(){
             return View();
         }
 
         [HttpPost]
         public IActionResult Salvar(Funcionario funcionario){
-            return Content($"Nome: {funcionario.Nome}\nSalário: {funcionario.Salario.ToString("F2", CultureInfo.InvariantCulture)}\nCPF: {funcionario.Cpf}");
+            database.Funcionarios.Add(funcionario);
+            database.SaveChanges();
+            return Content($"Nome: {funcionario.Nome}\nSalário: {funcionario.Salario.ToString("F2", CultureInfo.InvariantCulture)}\nCPF: {funcionario.Cpf}\n\nFuncionário cadastrado com sucesso!");
         }
     }
 }
