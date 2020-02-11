c6cca8bd2147aff86e95f3fa41cfd2f909ff01fd
Iury Chagas <iurychagas@icloud.com>
2020-02-11 10:04:08 -0300

    add Configurando Banco de Dados com ef
    dotnet ef --help
    Criando entidades e fazendo Migrations

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using School_of_net_MVC.Database;
+using School_of_net_MVC.Models;
+
 namespace School_of_net_MVC.Database {
     public class ApplicationDBContext : DbContext {
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
             
         }
---
+++ Models/Funcionario.cs
@@ -0,0 +1,7 @@
+namespace School_of_net_MVC.Models {
+    public class Funcionario {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Salario { get; set; }
+    }
+}
