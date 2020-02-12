49b26142e35ac5f174dd81f1a1b36e41b17c5288
Iury Chagas <iurychagas@icloud.com>
2020-02-12 14:36:39 -0300

    add Fluent API
    > Como realizar alterações na Tabela
    > Renomear uma tabela existente
    > Éspecificar regras para adicionar dados à tabela

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -14,5 +14,18 @@ namespace School_of_net_MVC.Database {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             optionsBuilder.UseLazyLoadingProxies();
         }
+
+        // Customização de Tabela com Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            //  para definir um novo nome para tabela!
+            modelBuilder.Entity<Produto>().ToTable("NovoNomeDaTabela");
+
+            // Para tornar obrigatório colocar no nome do produto,
+            //não sendo mais possível enviar um nome de produto vazio no BD.
+            //modelBuilder.Entity<Produto>().Property(p => p.Nome).IsRequired();
+
+            // Para definir uma quantidade maxima de caracteres que o nome do produto terá! até 100 caracteres!
+            modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100);
+        }
     }
 }
--- Migrations/20200212173147_ChangesTabelaProduto.Designer.cs
+++ Migrations/20200212173147_ChangesTabelaProduto.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using School_of_net_MVC.Database;
 
 namespace School_of_net_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200212173147_ChangesTabelaProduto")]
+    partial class ChangesTabelaProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
